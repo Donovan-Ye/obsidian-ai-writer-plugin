20a56d4cc346e9d0006cfc9bdbc948f1d76f28b4
ZhengjieYe <xxiaozhiyzj@gmail.com>
2024-04-08 12:00:58 +0800

    refactor(bump): sync version and other meta info to obsidian manifest.json

--- version-bump.mjs
+++ version-bump.mjs
@@ -2,11 +2,22 @@ import { readFileSync, writeFileSync } from 'node:fs'
 import process from 'node:process'
 
 const targetVersion = process.env.npm_package_version
+const targetName = process.env.npm_package_name
+const targetDescription = process.env.npm_package_description
+const targetAuthor = process.env.npm_package_author
+const targetUrl = process.env.npm_package_repository_url
+const targetFunding = process.env.npm_package_funding ?? 'waiting to be created...'
 
 // read minAppVersion from manifest.json and bump version to target version
 const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'))
 const { minAppVersion } = manifest
 manifest.version = targetVersion
+manifest.id = targetName
+manifest.name = targetName
+manifest.description = targetDescription
+manifest.author = targetAuthor
+manifest.authorUrl = targetUrl
+manifest.fundingUrl = targetFunding
 writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'))
 
 // update versions.json with target version and minAppVersion from manifest.json
