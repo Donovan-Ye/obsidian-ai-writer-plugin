e9dcf7ebd22c12ef6ecc89f3fe4b876d18d18ad7
ZhengjieYe <xxiaozhiyzj@gmail.com>
2024-04-09 10:31:59 +0800

    refactor: delete and refactor sample code

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,10 @@ import {
   Plugin,
   PluginSettingTab,
   Setting,
+  addIcon,
 } from 'obsidian'
-import { GPT_VIEW, GptView } from 'src/views/GptView'
+import { GPT_VIEW, GptView } from 'src/pages/GptView'
+import { GPTSettingTab } from './pages/SettingTab'
 
 interface MyPluginSettings {
   mySetting: string
@@ -22,16 +24,20 @@ const DEFAULT_SETTINGS: Partial<MyPluginSettings> = {
   mySetting: 'default',
 }
 
-const ALL_EMOJIS: Record<string, string> = {
-  ':+1:': '👍',
-  ':sunglasses:': '😎',
-  ':smile:': '😄',
-}
+const universalHint = 'Generate new article 📝'
 
 export default class MyPlugin extends Plugin {
   settings: MyPluginSettings
 
-  async activateView() {
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings)
+  }
+
+  async activateGPTView() {
     const { workspace } = this.app
 
     let leaf: WorkspaceLeaf | null = null
@@ -46,11 +52,6 @@ export default class MyPlugin extends Plugin {
       // in the right sidebar for it
       leaf = workspace.getRightLeaf(false)
       await leaf?.setViewState({ type: GPT_VIEW, active: true })
-
-      //   const file = this.app.vault.getFileByPath("example.md")
-      //   if (file) {
-      //     await leaf?.openFile(file)
-      //   }
     }
 
     // "Reveal" the leaf in case it is in a collapsed sidebar
@@ -59,32 +60,19 @@ export default class MyPlugin extends Plugin {
   }
 
   async onload() {
-    this.registerMarkdownPostProcessor((element) => {
-      const codeblocks = element.findAll('code')
-
-      for (const codeblock of codeblocks) {
-        const text = codeblock.textContent?.trim() ?? ''
-        if (text[0] === ':' && text[text.length - 1] === ':') {
-          const emojiEl = codeblock.createSpan({
-            text: ALL_EMOJIS[text] ?? text,
-          })
-          codeblock.replaceWith(emojiEl)
-        }
-      }
-    })
+    await this.loadSettings()
 
     this.registerView(GPT_VIEW, leaf => new GptView(leaf))
 
-    this.addRibbonIcon('dice', 'Activate view', () => {
-      this.activateView()
+    this.addRibbonIcon('message-square', 'Activate GPT view', () => {
+      this.activateGPTView()
     })
 
     this.registerEvent(
       this.app.workspace.on('file-menu', (menu, file) => {
         menu.addItem((item) => {
           item
-            .setTitle('Print file path 👈')
-            .setIcon('document')
+            .setTitle(universalHint)
             .onClick(async () => {
               new Notice(file.path)
             })
@@ -96,7 +84,7 @@ export default class MyPlugin extends Plugin {
       this.app.workspace.on('editor-menu', (menu, editor, view) => {
         menu.addItem((item) => {
           item
-            .setTitle('Print file path 👈')
+            .setTitle(universalHint)
             .setIcon('document')
             .onClick(async () => {
               new Notice(view.file?.path ?? 'No file path')
@@ -105,152 +93,16 @@ export default class MyPlugin extends Plugin {
       }),
     )
 
-    this.addRibbonIcon('dice', 'Open menu', (event) => {
-      const menu = new Menu()
-
-      menu.addItem(item =>
-        item
-          .setTitle('Copy')
-          .setIcon('documents')
-          .onClick(() => {
-            new Notice('Copied')
-          }),
-      )
-
-      menu.addItem(item =>
-        item
-          .setTitle('Paste')
-          .setIcon('paste')
-          .onClick(() => {
-            new Notice('Pasted')
-          }),
-      )
-
-      menu.showAtMouseEvent(event)
-    })
-
-    await this.loadSettings()
-
-    // This creates an icon in the left ribbon.
-    const ribbonIconEl = this.addRibbonIcon(
-      'dice',
-      'Sample Plugin',
-      () => {
-        // Called when the user clicks the icon.
-        new Notice('This is a notice!')
-      },
-    )
-    // Perform additional things with the ribbon
-    ribbonIconEl.addClass('my-plugin-ribbon-class')
-
-    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-    const statusBarItemEl = this.addStatusBarItem()
-    statusBarItemEl.setText('Status Bar Text')
-
     // This adds a simple command that can be triggered anywhere
     this.addCommand({
-      id: 'open-sample-modal-simple',
-      name: 'Open sample modal (simple)',
-      callback: () => {
-        new SampleModal(this.app).open()
-      },
-    })
-    // This adds an editor command that can perform some operation on the current editor instance
-    this.addCommand({
-      id: 'sample-editor-command',
-      name: 'Sample editor command',
-      editorCallback: (editor: Editor) => {
-        editor.replaceSelection('Sample Editor Command')
-      },
-    })
-    // This adds a complex command that can check whether the current state of the app allows execution of the command
-    this.addCommand({
-      id: 'open-sample-modal-complex',
-      name: 'Open sample modal (complex)',
-      checkCallback: (checking: boolean) => {
-        // Conditions to check
-        const markdownView
-          = this.app.workspace.getActiveViewOfType(MarkdownView)
-        if (markdownView) {
-          // If checking is true, we're simply "checking" if the command can be run.
-          // If checking is false, then we want to actually perform the operation.
-          if (!checking)
-            new SampleModal(this.app).open()
-
-          // This command will only show up in Command Palette when the check function returns true
-          return true
-        }
-      },
+      id: 'generate-new-article',
+      name: universalHint,
+      callback: () => { },
     })
 
     // This adds a settings tab so the user can configure various aspects of the plugin
-    this.addSettingTab(new SampleSettingTab(this.app, this))
-
-    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-    // Using this function will automatically remove the event listener when this plugin is disabled.
-    this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-      // eslint-disable-next-line no-console
-      console.log('click', evt)
-    })
-
-    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-    this.registerInterval(
-      // eslint-disable-next-line no-console
-      window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000),
-    )
+    this.addSettingTab(new GPTSettingTab(this.app, this))
   }
 
   onunload() {}
-
-  async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
-  }
-
-  async saveSettings() {
-    await this.saveData(this.settings)
-  }
-}
-
-class SampleModal extends Modal {
-  constructor(app: App) {
-    super(app)
-  }
-
-  onOpen() {
-    const { contentEl } = this
-    contentEl.setText('Woah!')
-  }
-
-  onClose() {
-    const { contentEl } = this
-    contentEl.empty()
-  }
-}
-
-class SampleSettingTab extends PluginSettingTab {
-  plugin: MyPlugin
-
-  constructor(app: App, plugin: MyPlugin) {
-    super(app, plugin)
-    this.plugin = plugin
-  }
-
-  display(): void {
-    const { containerEl } = this
-
-    containerEl.empty()
-
-    new Setting(containerEl)
-      .setName('Setting #1')
-      .setDesc('It\'s a secret')
-      .addText(text =>
-        text
-          .setPlaceholder('Enter your secret')
-          .setValue(this.plugin.settings.mySetting)
-          .onChange(async (value) => {
-            this.plugin.settings.mySetting = value
-            await this.plugin.saveSettings()
-          }),
-      )
-  }
 }
