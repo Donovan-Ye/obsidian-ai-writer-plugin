8d532838b1965a5bed213ab7ac3a359557c751fc
ZhengjieYe <xxiaozhiyzj@gmail.com>
2024-04-10 13:18:58 +0800

    feat: notice errror message in GPTView

--- src/pages/GptView/Artilcle.tsx
+++ src/pages/GptView/Artilcle.tsx
@@ -1,21 +1,27 @@
 import { useEffect, useState } from 'react'
 import LLMProvider from 'src/llmProvider'
+import { Notice } from 'obsidian'
 import type { ArticleProps } from './types'
 
 function Article({ title, content, getSettings }: ArticleProps) {
   const [messages, setMessages] = useState('')
 
   async function requestGPT() {
-    const provider = new LLMProvider(getSettings())
+    try {
+      const provider = new LLMProvider(getSettings())
 
-    const stream = await provider.chat(
-      {
-        messages: [{ role: 'user', content: '你好' }],
-        stream: true,
-      },
-    )
-    for await (const chunk of stream)
-      setMessages(prevMsg => prevMsg + (chunk?.choices?.[0]?.delta?.content ?? ''))
+      const stream = await provider.chat(
+        {
+          messages: [{ role: 'user', content: '你好' }],
+          stream: true,
+        },
+      )
+      for await (const chunk of stream)
+        setMessages(prevMsg => prevMsg + (chunk?.choices?.[0]?.delta?.content ?? ''))
+    }
+    catch (err) {
+      new Notice(err.message)
+    }
   }
 
   useEffect(() => {
