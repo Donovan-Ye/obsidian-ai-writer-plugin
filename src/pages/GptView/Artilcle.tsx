a4a5a2255a9a99405c37b336403a24323c9d1c10
ZhengjieYe <xxiaozhiyzj@gmail.com>
2024-04-11 12:10:16 +0800

    feat: show generate article in code editor

--- src/pages/GptView/Artilcle.tsx
+++ src/pages/GptView/Artilcle.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import LLMProvider from 'src/llmProvider'
 import { Notice } from 'obsidian'
 import { getPrompt } from 'src/prompt'
+import CodeMirror from '@uiw/react-codemirror'
+import { okaidia } from '@uiw/codemirror-theme-okaidia'
 import type { ArticleProps } from './types'
 
 function Article({
@@ -9,10 +11,11 @@ function Article({
   content,
   getSettings,
 }: ArticleProps) {
-  const [messages, setMessages] = useState('')
+  const [articleContent, setArticleContent] = useState('')
 
   async function requestGPT() {
     try {
+      setArticleContent('')
       const provider = new LLMProvider(getSettings())
 
       const prompt = getPrompt({
@@ -28,7 +31,7 @@ function Article({
         },
       )
       for await (const chunk of stream)
-        setMessages(prevMsg => prevMsg + (chunk?.choices?.[0]?.delta?.content ?? ''))
+        setArticleContent(prevMsg => prevMsg + (chunk?.choices?.[0]?.delta?.content ?? ''))
     }
     catch (err) {
       new Notice(err.message)
@@ -40,10 +43,38 @@ function Article({
   }, [title, content])
 
   return (
-    <div>
+    <div id="ai-writer-plugin">
       <h1>{title}</h1>
 
-      <p>{messages}</p>
+      <CodeMirror
+        value={articleContent}
+        onCreateEditor={
+          () => {
+            const cmContent = document.querySelector('#ai-writer-plugin .cm-content')
+            if (cmContent) {
+              (cmContent as HTMLElement).style.whiteSpace = 'break-spaces';
+              (cmContent as HTMLElement).style.wordBreak = 'break-word';
+              (cmContent as HTMLElement).style.overflowWrap = 'anywhere'
+
+              const wrapper = document.createElement('div')
+              wrapper.style.display = 'flex'
+              wrapper.style.flexDirection = 'column'
+              wrapper.style.alignItems = 'stretch'
+              wrapper.style.width = '100%'
+              wrapper.style.minHeight = '100%'
+
+              cmContent.parentNode?.insertBefore(wrapper, cmContent)
+              wrapper.appendChild(cmContent)
+            }
+          }
+        }
+        height="100%"
+        className="test"
+        theme={okaidia}
+        onChange={(value) => {
+          setArticleContent(value)
+        }}
+      />
     </div>
   )
 }
