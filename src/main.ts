2dbf6cd6ee6de93d2827ec42babbb686f23754f1
ZhengjieYe <xxiaozhiyzj@gmail.com>
2024-04-13 16:18:21 +0800

    build: remove import css to avoid generating main.css when building

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { t } from 'i18next'
 import { DEFAULT_SETTINGS, GPTSettingTab } from './pages/SettingTab'
 import type { GPTSettings } from './pages/SettingTab/types'
 import { ArticleFormatModal } from './pages/ArticleFormatModal'
-import './main.css'
 // internationalization
 import './i18n'
 
